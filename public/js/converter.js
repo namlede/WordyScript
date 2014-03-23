8b28abc20ac5ba63a6ec4e04ce32e1e456ef4281
Powerroff <aaron36berger@gmail.com>
2014-03-23 03:24:31 -0400

    Update converter.js

--- public/js/converter.js
+++ public/js/converter.js
@@ -27,6 +27,8 @@ function replacer(a){
 	a=a.replace(/define new function /g, "def ");
 	a=a.replace(/define function /g, "def ");
 	a=a.replace(/define /g, "def ");
+	a=a.replace(/new function /g, "def ");
+	a=a.replace(/function /g, "def ");
 	
     a =a.replace(/ equals /g, "==");
 
