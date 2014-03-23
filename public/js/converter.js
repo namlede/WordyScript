e7c0c0f7f171d608b2fd7efbd422b7653bb60ef1
Ben Edelman <benedelman100@gmail.com>
2014-03-23 00:28:01 -0400

    fixed bug

--- public/js/converter.js
+++ public/js/converter.js
@@ -21,7 +21,7 @@ function replacer(a){
 	a =a.replace(/ is true/g, " == true");
 	a =a.replace(/ is false/g, "== false");
 	a =a.replace(/ equal to /g, "=");
-    	a =a.replace(/ equals /g, "=");
+    a =a.replace(/ equals /g, "=");
 
 	a =a.replace(/ to /g, "=");
     
@@ -73,7 +73,7 @@ function replacer(a){
 			if(stmt){
 				stmt=stmt[0];
 				var stmt=a[i].match(/\swhile\s(.*)\S/)[0];
-				var bit=a[i].match(/\swhile\s/).index+1l
+				var bit=a[i].match(/\swhile\s/).index+1;
 				stmt2=stmt.substring(1,bit)+stmt.substring(bit+5);
 				var args=stmt2.replace(":","");
 				a[i]=a[i].replace(stmt,"while ("+args+"):");
