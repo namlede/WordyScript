d37dc45abfc2d842912e55029a2c9012bf18be64
Powerroff <aaron36berger@gmail.com>
2014-03-23 04:36:31 -0400

    again, +=, -=, etc

--- public/js/converter.js
+++ public/js/converter.js
@@ -107,19 +107,19 @@ function replacer(a){
 				a[i]+=" :";
 		}
 		
-		if (a[i].match("increase ")&&a[i].match(" by ")) {
+		if (a[i].match(/increase /) && a[i].match(/ by /)) {
 			a[i].replace("increase","")
 			a[i].replace("by","+=")
 		}
-		if (a[i].match("decrease ")&&a[i].match(" by ")) {
+		if (a[i].match(/decrease /) && a[i].match(/ by /)) {
 			a[i].replace("decrease","")
 			a[i].replace("by","-=")
 		}
-		if (a[i].match("multiply ")&&a[i].match(" by ")) {
+		if (a[i].match(/multiply /) && a[i].match(/ by /)) {
 			a[i].replace("multiply","")
 			a[i].replace("by","*=")
 		}
-		if (a[i].match("divide ")&&a[i].match(" by ")) {
+		if (a[i].match(/divide /) && a[i].match(/ by /)) {
 			a[i].replace("divide","")
 			a[i].replace("by","/=")
 		}
