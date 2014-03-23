58fd2e13ce7264deb6f42d0d1cb37e1f129131df
Ben Edelman <benedelman100@gmail.com>
2014-03-23 10:12:07 -0400

    deleted .html file, trying to get sockets working, added favicon

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ storage.setItem("bob","hi");
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(express.favicon());
+app.use(express.favicon(path.join(__dirname, 'public/images/favicon.ico')));
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
